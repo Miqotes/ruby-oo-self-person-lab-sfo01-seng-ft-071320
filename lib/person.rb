aea7433450d86791757180f81dfc71e058a50ce3
Ashlee Barksdale <ashleelennab@gmail.com>
2020-07-16 01:07:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
 # your code goes here
+#Class of Person
+
+#Each person has a name, bank account, happiness, hygeine 
+#Set these 
+  #Each person starts with $25 dollars in bank 
+  #Each person starts with 8 happiness points
+  #Each person starts with 8 hygeine points
+  
+class Person 
+  
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygeine
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25 
+    @happiness = 8
+    @hygeine = 8
+    
+  end
+  
+  def happy=(num)
+    #if num > 10 
+     # @happiness = 10 
+    #end
+  end   
+    
+end
